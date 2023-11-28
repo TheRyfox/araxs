1f22d7c8056f2ee08abc474459fc7928bc04e865
TheRyfox <126391184+TheRyfox@users.noreply.github.com>
2023-11-28 23:11:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const loadDataFromLocalstorage = () => {
 
     const defaultText = 
     `<div class="default-text">
-    <h1>ChatGPT Clone</h1>
-    <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
+    <h1>Araxs IA</h1>
+    <p>Comienza una conversación y explora el poder de la IA<br> Tu historial de chat se mostrará aquí.</p>
 </div>`
 
     chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
@@ -81,7 +81,7 @@ const showTypingAnimation = () => {
     // Display the typing animation and call the getChatResponse function
     const html = `<div class="chat-content">
                     <div class="chat-details">
-                        <img id="chatbot-img">
+                        <img src="https://media.discordapp.net/attachments/1148673347392127046/1178448186763923547/imagen_2023-11-26_222317809_waifu2x_photo_noise3_scale.png?ex=65762e66&is=6563b966&hm=eb15638ae46f267d6fc14823f54990815d73bccb1632d8537def78d8d3b9843a&=&format=webp&width=671&height=671">
                         <div class="typing-animation">
                             <div class="typing-dot" style="--delay: 0.2s"></div>
                             <div class="typing-dot" style="--delay: 0.3s"></div>
@@ -107,7 +107,7 @@ const handleOutgoingChat = () => {
 
     const html = `<div class="chat-content">
                     <div class="chat-details">
-                        <img id="user-img">
+                        <img src="https://media.discordapp.net/attachments/1134429684973707324/1178144286877950053/OIG.png?ex=6575135e&is=65629e5e&hm=e0a382619b5a8f093641038e06fd48c7bcfda5d673d9bcb5167b71fd87a7f8c5&=&format=webp&width=297&height=297">
                         <p>${userText}</p>
                     </div>
                 </div>`;
